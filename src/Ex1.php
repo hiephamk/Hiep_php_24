5942550075d131eb140dfd245f2cc36546eed9e7
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-01-26 11:32:06 +0200

    g

--- src/Ex1.php
+++ src/Ex1.php
@@ -11,5 +11,26 @@
     <?php
     echo "Hello world! My name is \"Hiep\"";
     ?>
+    <h3>
+    Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an (heading 4) element.
+    </h3>
+    <?php
+        $title = "PHP is interesting.";
+        echo "<h4>$title</h4>";
+    ?>
+    <h3>
+    Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. 
+    These variables represent the grades of three students in the course. 
+    To display this information, create an HTML table within your PHP code. 
+    The table should be structured with columns 
+    for Serial Number (S.n.), Name, and Grade, and it should look like this:
+    </h3>
+    <?php
+        $g1 = 5;
+        $g2 = 4;
+        $g3 = 5;
+
+    ?>
+
 </body>
 </html>
