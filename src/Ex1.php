3cc921d8f1eac16ea7d7836d8e4fcc2bac7f2efd
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-01-26 11:23:52 +0200

    d

---
+++ src/Ex1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Hiep</title>
+</head>
+<body>
+    <h3> 3.1 Write PHP code to output the following message: Hello world! My name is "Hiep"
+    </h3>
+    <?php
+    echo "Hello world! My name is \"Hiep\"";
+    ?>
+</body>
+</html>
