8f640c6167956c440ce7bd9d21fe64ad3d590769
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-01-26 11:07:57 +0200

    Add files via upload

---
+++ src/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "<h1>hello world</h1>";
+echo "<h1> that is the frist lesson php";
+?>
+</body>
+</html>
