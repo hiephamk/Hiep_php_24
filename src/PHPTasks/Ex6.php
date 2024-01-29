7287a2cc37f56b996c115a76a3959db47f3dd9b9
hiephamk <142878332+hiephamk@users.noreply.github.com>
2024-01-29 11:43:38 +0200

    dd

--- src/PHPTasks/Ex6.php
+++ src/PHPTasks/Ex6.php
@@ -1,4 +1,6 @@
 <?php $title = "Ex6.php"; ?>
 <?php include 'header.php'; ?>
-<div class="main-page container"></div>
+<div class="main-page container">
+    <p>Put the contents here</p>
+</div>
 <?php include "footer.php"; ?>
--- src/PHPTasks/Ex7.php
+++ src/PHPTasks/Ex7.php
@@ -1,6 +1,6 @@
 <?php $title = "Ex7.php"; ?>
 <?php include 'header.php'; ?>
 <div class="main-page container">
-
+    <p>Put the contents here</p>
 </div>
 <?php include "footer.php"; ?>
